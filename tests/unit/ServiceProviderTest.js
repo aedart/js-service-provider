c3ef828ec74424a0a6ed86298f1753b28f1a3106
Alin Eugen Deac <aedart@gmail.com>
2016-11-05 11:56:18 +0100

    Add unit test for ServiceProvider

---
+++ tests/unit/ServiceProviderTest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import ServiceProvider from './../../src/ServiceProvider';
+import TestHelper from './../helpers/TestHelper';
+
+describe('Service Provider', function(){
+
+    it('fails when attempting to create instance', function(){
+        let f = () => {
+            return new ServiceProvider({});
+        };
+
+        expect(f).toThrowError(TypeError);
+    });
+
+    it('can set and get ioc instance', function () {
+        let ioc = TestHelper.ioc;
+        let provider = TestHelper.makeDymmyServiceProvider(ioc);
+
+        expect(provider.ioc).toBe(ioc);
+    });
+});
