905b59d5c4084221a82a4e96aa8212dd33ec15c0
Alin Eugen Deac <aedart@gmail.com>
2016-11-06 14:04:29 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// Default export - Service provider
+import ServiceProvider from './src/ServiceProvider';
+export default ServiceProvider;
+
+// Aggregate Service Provider
+import AggregateServiceProvider from './src/AggregateServiceProvider';
+export { AggregateServiceProvider };
+
+// Registrar
+import Registrar from './src/Registrar';
+export { Registrar };
+
+import RegistrarServiceProvider from './src/Providers/RegistrarServiceProvider';
+import { REGISTRAR_CLASS } from './src/Providers/RegistrarServiceProvider';
+export { RegistrarServiceProvider };
+export { REGISTRAR_CLASS };
+
+// Mixins
+import RegistrarAware from './src/Mixins/RegistrarAware';
+export { RegistrarAware };
+
+// Facades
+import IoCFacade from './src/Facades/IoCFacade';
+export { IoCFacade };
+
