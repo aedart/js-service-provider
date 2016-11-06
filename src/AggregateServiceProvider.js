3bcf0500a3d480eeef9452044e2967283d806977
Alin Eugen Deac <aedart@gmail.com>
2016-11-06 13:57:15 +0100

    Add AggregateServiceProvider

---
+++ src/AggregateServiceProvider.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import ServiceProvider from './ServiceProvider';
+import RegistrarAware from './Mixins/RegistrarAware';
+import { mix } from 'mixwith/src/mixwith';
+
+/**
+ * providers symbol
+ *
+ * @type {Symbol}
+ * @private
+ */
+const _providers = Symbol('providers');
+
+/**
+ * Aggregate Service Provider
+ *
+ * @extends ServiceProvider
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class AggregateServiceProvider extends mix(ServiceProvider).with(RegistrarAware) {
+
+    constructor(ioc){
+        super(ioc);
+        this.providers = [];
+    }
+
+    /**
+     * Set providers
+     *
+     * @param {Array.<ServiceProvider|function>} list List of service providers
+     */
+    set providers(list) {
+        this[_providers] = list;
+    }
+
+    /**
+     * Get providers
+     *
+     * @return {Array.<ServiceProvider|function>} List of service providers
+     */
+    get providers() {
+        return this[_providers];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    register(){
+        this.registrar.registerProviders(this.providers);
+    }
+}
+
+export default AggregateServiceProvider;
