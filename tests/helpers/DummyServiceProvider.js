98f9cdc7d7d2664d23d92762fb60fbd2b0f9bf09
Alin Eugen Deac <aedart@gmail.com>
2016-11-06 10:43:31 +0100

    Add has registered / has booted states
    Helper methods to make it a bit easier to test.

--- tests/helpers/DummyServiceProvider.js
+++ tests/helpers/DummyServiceProvider.js
@@ -3,7 +3,25 @@
 import ServiceProvider from './../../src/ServiceProvider';
 
 /**
+ * has Registered symbol
+ *
+ * @type {Symbol}
+ * @private
+ */
+const _hasRegistered = Symbol('has-registered');
+
+/**
+ * has Booted symbol
+ *
+ * @type {Symbol}
+ * @private
+ */
+const _hasBooted = Symbol('has-booted');
+
+/**
  * Dummy Service Provider
+ *
+ * @description FOR TESTING ONLY - does not register nor boot anything...
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  */
@@ -11,8 +29,63 @@ class DummyServiceProvider extends ServiceProvider{
 
     constructor(ioc){
         super(ioc);
+        this.hasRegistered = false;
+        this.hasBooted = false;
     }
 
+    /**
+     * Set has Registered
+     *
+     * @param {boolean} hasRegistered State of this provider
+     */
+    set hasRegistered(hasRegistered) {
+        this[_hasRegistered] = hasRegistered;
+    }
+
+    /**
+     * Get has Registered
+     *
+     * @return {boolean} State of this provider
+     */
+    get hasRegistered() {
+        return this[_hasRegistered];
+    }
+
+    /**
+     * Set has Booted
+     *
+     * @param {boolean} hasBooted State of this provider
+     */
+    set hasBooted(hasBooted) {
+        this[_hasBooted] = hasBooted;
+    }
+
+    /**
+     * Get has Booted
+     *
+     * @return {boolean} State of this provider
+     */
+    get hasBooted() {
+        return this[_hasBooted];
+    }
+
+    /**
+     * Register all of this provider's services
+     *
+     * @return {void}
+     */
+    register(){
+        this.hasRegistered = true;
+    }
+
+    /**
+     * Boot this provider
+     *
+     * @return {void}
+     */
+    boot(){
+        this.hasBooted = true;
+    }
 }
 
 export default DummyServiceProvider;
