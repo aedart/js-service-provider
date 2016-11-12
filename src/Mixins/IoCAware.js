4f1ad5569ec994950681724c8f0dd455cc8b1a60
Alin Eugen Deac <aedart@gmail.com>
2016-11-12 12:08:05 +0100

    Add IoC aware component

---
+++ src/Mixins/IoCAware.js
@@ -0,0 +1,59 @@
+'use strict';
+
+import IoCFacade from './../Facades/IoCFacade';
+import {Mixin} from 'mixwith/src/mixwith';
+
+/**
+ * ioc symbol
+ *
+ * @type {Symbol}
+ * @private
+ */
+const _ioc = Symbol('ioc');
+
+/**
+ * IoC Aware Mixin
+ *
+ * @return {IoCAware}
+ */
+export default Mixin((superClass) => class IoCAware extends superClass {
+
+    /**
+     * Set ioc
+     *
+     * @param {Container|null} instance IoC Service Container instance
+     */
+    set ioc(instance) {
+        this[_ioc] = instance;
+    }
+
+    /**
+     * Get ioc
+     *
+     * @return {Container|null} IoC Service Container instance
+     */
+    get ioc() {
+        if (!this.hasIoc()) {
+            this.ioc = this.defaultIoc;
+        }
+        return this[_ioc];
+    }
+
+    /**
+     * Check if "ioc" has been set
+     *
+     * @return {boolean}
+     */
+    hasIoc() {
+        return (this[_ioc] !== undefined && this[_ioc] !== null);
+    }
+
+    /**
+     * Get a default "ioc"
+     *
+     * @return {Container|null} A default "ioc" value or null if none is available
+     */
+    get defaultIoc() {
+        return IoCFacade.make('ioc');
+    }
+});
---
+++ tests/integration/mixins/IoCAwareTest.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import IoCAware from '../../../src/Mixins/IoCAware';
+import TestHelper from '../../helpers/TestHelper';
+import Facade from '@aedart/js-facade';
+import IoC from '@aedart/js-ioc';
+import { mix } from 'mixwith/src/mixwith';
+
+describe('IoC Aware Mixin', function(){
+
+    afterEach(() => {
+        IoC.flush();
+
+        Facade.ioc = null;
+        Facade.clearResolvedInstances();
+    });
+
+    /********************************************************************
+     * Helpers
+     *******************************************************************/
+
+    class DummyClass extends mix(Object).with(IoCAware) {}
+
+    /********************************************************************
+     * Actual tests
+     *******************************************************************/
+
+    it('has null as default registrar', function () {
+        let dummy = new DummyClass();
+
+        expect(dummy.ioc).toBeNull();
+    });
+
+    it('can get and set ioc', function () {
+        let dummy = new DummyClass();
+
+        let ioc = IoC;
+
+        dummy.ioc = ioc;
+
+        expect(dummy.ioc).toBe(ioc);
+    });
+
+    it('returns IoC, when IoC is available', function () {
+        Facade.ioc = IoC;
+        IoC.instances.set('ioc', IoC);
+
+        let dummy = new DummyClass();
+
+        expect(dummy.ioc).not.toBeNull();
+        expect(dummy.ioc).toBe(IoC);
+    });
+});
